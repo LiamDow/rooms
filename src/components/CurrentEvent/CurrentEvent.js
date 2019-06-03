dbc1c3efae84aab1e8e365918ac226c52c36956e
LiamDow <51240741+LiamDow@users.noreply.github.com>
2019-06-03 16:17:42 +0100

    Update CurrentEvent.js
    translating spanish to english

--- src/components/CurrentEvent/CurrentEvent.js
+++ src/components/CurrentEvent/CurrentEvent.js
@@ -30,32 +30,32 @@ export default class CurrentEvent extends PureComponent {
         <div className="CurrentEvent is-available">
           { remainingMinutes <= 120 ?
               remainingMinutes > 0 ?
-                <div className="time">Por {remainingMinutes} minutos</div>
+                <div className="time">For {remainingMinutes} minutes</div>
                 :
-                <div className="time">Por {remainingSeconds} segundos</div>
+                <div className="time">For {remainingSeconds} seconds</div>
             :
             now.isSame(event.end, 'day') ?
-              <div className="time">Hasta las {moment(event.end).format('HH:mm')}hs</div>
+              <div className="time">Until {moment(event.end).format('HH:mm')}hs</div>
               :
               now.isSame(prevEvent.end, 'day') ?
-                <div className="time">El resto del día</div>
+                <div className="time">The rest of the day</div>
                 :
-                <div className="time">Todo el día</div>
+                <div className="time">All day</div>
           }
 
-          <div className="title">Sala Libre</div>
+          <div className="title">Free Room</div>
 
           { remainingMinutes >= 5 &&
             <div className="actions">
               <button className="Button" onClick={() => onBook({
                 start: now.clone().startOf('minute'),
                 end: moment.min(now.clone().add(15, 'minutes').endOf('minute'), moment(event.end))
-              })}>{`Reservar ${Math.min(15, remainingMinutes)} minutos`}</button>
+              })}>{`Reserve ${Math.min(15, remainingMinutes)} minutes`}</button>
 
               <a className="Link" onClick={() => onBook({
                 start: now.clone().startOf('minute'),
                 end: moment.min(now.clone().add(60, 'minutes').endOf('minute'), moment(event.end))
-              })}>{ remainingMinutes >= 60 ? `Reservar una hora` : `Reservar hasta la próxima reunión` }</a>
+              })}>{ remainingMinutes >= 60 ? `Reserve for an hour` : `Reserve until the next meeting` }</a>
             </div>
           }
 
@@ -68,24 +68,24 @@ export default class CurrentEvent extends PureComponent {
         {now.isSame(event.end, 'day') ?
           remainingMinutes < 60 ?
             remainingMinutes > 0 ?
-              <div className="time">Quedan {remainingMinutes} minutos</div>
+              <div className="time">There are {remainingMinutes} minutes left</div>
               :
-              <div className="time">Quedan {remainingSeconds} segundos</div>
+              <div className="time">There are {remainingSeconds} seconds left</div>
             :
-            <div className="time">Hasta las {moment(event.end).format('HH:mm')}hs</div>
+            <div className="time">Until {moment(event.end).format('HH:mm')}hs</div>
           :
-          <div className="time">El resto del día</div>
+          <div className="time">The rest of the day</div>
         }
 
-        <div className={`title ${event.summary && event.summary.length > 30 ? 'small' : ''} `}>{event.summary ? event.summary : 'Evento Privado'}</div>
+        <div className={`title ${event.summary && event.summary.length > 30 ? 'small' : ''} `}>{event.summary ? event.summary : 'Private Event'}</div>
 
         {event.organizer && ! event.isFlashEvent && 
-          <div className="booker">Agendado por <strong>{event.organizer.displayName || event.organizer.email}</strong></div>
+          <div className="booker">Booked by <strong>{event.organizer.displayName || event.organizer.email}</strong></div>
         }
 
         {event.isFlashEvent &&
           <div className={`actions ${showRemoveButton ? 'is-visible' : 'is-hidden'} `}>
-            <a className="Link" onClick={() => showRemoveButton && onRemoveBooking(event)}>Liberar Reserva</a>
+            <a className="Link" onClick={() => showRemoveButton && onRemoveBooking(event)}>Release Room</a>
           </div>
         }
       </div>
